14364f87459768be6391b44b9446b85e76bebfee
Steffan Venema <steffan.venema@bakerware.nl>
2023-11-08 22:17:27 +0100

    Added Login

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::post('/sanctum/token', function (Request $request) {
 
     if (!$user || !Hash::check($request->password, $user->password)) {
         throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
+            'username' => ['The provided credentials are incorrect.'],
         ]);
     }
 
