7be72d13c8d893e44f8bacb33f0a4454a8745873
Aurora <leahvenema@hotmail.com>
2024-10-16 22:22:05 +0200

    Flush Settings

--- src/Application/Service/SettingApplicationService.php
+++ src/Application/Service/SettingApplicationService.php
@@ -5,16 +5,22 @@ namespace App\Application\Service;
 use App\Application\Request\SettingRequest;
 use App\Domain\Entity\Setting;
 use App\Domain\Service\SettingDomainService;
+use Doctrine\ORM\EntityManagerInterface;
 
 readonly class SettingApplicationService
 {
     public function __construct(
-        private SettingDomainService $settingDomainService
+        private SettingDomainService $settingDomainService,
+        private EntityManagerInterface $entityManager
     ) {
     }
 
     public function setSetting(SettingRequest $settingRequest): Setting
     {
-        return $this->settingDomainService->setSetting($settingRequest->name, $settingRequest->value);
+        $setting = $this->settingDomainService->setSetting($settingRequest->name, $settingRequest->value);
+
+        $this->entityManager->flush();
+
+        return $setting;
     }
 }
