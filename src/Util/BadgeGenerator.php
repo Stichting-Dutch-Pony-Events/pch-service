a6993332796ec56525a50fade3b200341c036a92
Leah <leahvenema@hotmail.com>
2025-10-22 12:53:56 +0200

    Throw more usefull error when struggling with the badge

--- src/Util/BadgeGenerator.php
+++ src/Util/BadgeGenerator.php
@@ -8,6 +8,7 @@ use App\Util\Exceptions\Exception\Common\InvalidInputException;
 use App\Util\Exceptions\Exception\Entity\EntityNotFoundException;
 use Exception;
 use GdImage;
+use GuzzleHttp\Exception\ClientException;
 use JsonException;
 use jucksearm\barcode\QRcode;
 use Symfony\Component\Filesystem\Filesystem;
@@ -31,21 +32,26 @@ class BadgeGenerator
     {
         $product = $attendee->getOverrideBadgeProduct() ?? $attendee->getProduct();
 
-        $imagePath = __DIR__ . '/../../assets/badges/' . $product->getPretixId() . '.png';
-        $dataPath = __DIR__ . '/../../assets/badges/' . $product->getPretixId() . '.json';
+        $imagePath = __DIR__.'/../../assets/badges/'.$product->getPretixId().'.png';
+        $dataPath  = __DIR__.'/../../assets/badges/'.$product->getPretixId().'.json';
 
         if (!file_exists($imagePath) || !file_exists($dataPath)) {
-            throw new EntityNotFoundException('Badges not found for: ' . $product->getName());
+            throw new EntityNotFoundException('Badges not found for: '.$product->getName());
         }
 
         $data = json_decode($this->filesystem->readFile($dataPath), false, 512, JSON_THROW_ON_ERROR);
 
-        $orderPosition = $this->orderRepository->getOrderPosition($attendee->getTicketId());
+        try {
+            $orderPosition = $this->orderRepository->getOrderPosition($attendee->getTicketId());
+        } catch (ClientException $e) {
+            throw new EntityNotFoundException('Badges not found for: '.$attendee->getTicketId());
+        }
+
         $ocImageUrl = $orderPosition->getAnswer('OC-IMAGE');
 
         $image = imagecreatefrompng($imagePath);
         if ($image === false) {
-            throw new InvalidInputException('Could not create image from badge template: ' . $imagePath);
+            throw new InvalidInputException('Could not create image from badge template: '.$imagePath);
         }
 
         imagesavealpha($image, true);
@@ -54,7 +60,7 @@ class BadgeGenerator
 
         if ($ocImageUrl) {
             try {
-                $tmpPath = __DIR__ . '/../../var/tmp/' . $product->getPretixId();
+                $tmpPath = __DIR__.'/../../var/tmp/'.$product->getPretixId();
 
                 $tmpPath = $this->orderRepository->downloadImage($ocImageUrl, $tmpPath);
 
@@ -71,8 +77,8 @@ class BadgeGenerator
                 if ($ocImage) {
                     imagesavealpha($ocImage, true);
                     $ocImage = $this->resizeImage($ocImage, $data->profileWidth, $data->profileHeight);
-                    $posX = (int)round(($data->profileWidth - imagesx($ocImage)) / 2) + $data->profileX;
-                    $posY = (int)round(($data->profileHeight - imagesy($ocImage)) / 2) + $data->profileY;
+                    $posX    = (int)round(($data->profileWidth - imagesx($ocImage)) / 2) + $data->profileX;
+                    $posY    = (int)round(($data->profileHeight - imagesy($ocImage)) / 2) + $data->profileY;
 
                     imagecopy($image, $ocImage, $posX, $posY, 0, 0, imagesx($ocImage), imagesy($ocImage));
 
@@ -80,29 +86,29 @@ class BadgeGenerator
                 }
             } catch (Exception $e) {
                 // If the image could not be created, we will use the default image
-                error_log('Error creating OC image: ' . $e->getMessage());
+                error_log('Error creating OC image: '.$e->getMessage());
             }
         }
 
         if ($useDefault) {
-            $ocPath = __DIR__ . '/../../assets/badges/unknown.png';
+            $ocPath  = __DIR__.'/../../assets/badges/unknown.png';
             $ocImage = imagecreatefrompng($ocPath);
             if ($ocImage === false) {
-                throw new InvalidInputException('Could not create image from default badge template: ' . $ocPath);
+                throw new InvalidInputException('Could not create image from default badge template: '.$ocPath);
             }
 
             imagesavealpha($ocImage, true);
             $ocImage = $this->resizeImage($ocImage, $data->profileWidth, $data->profileHeight);
-            $posX = ($data->profileWidth - imagesx($ocImage)) / 2 + $data->profileX;
-            $posY = ($data->profileHeight - imagesy($ocImage)) / 2 + $data->profileY;
+            $posX    = ($data->profileWidth - imagesx($ocImage)) / 2 + $data->profileX;
+            $posY    = ($data->profileHeight - imagesy($ocImage)) / 2 + $data->profileY;
 
             imagecopy($image, $ocImage, $posX, $posY, 0, 0, imagesx($ocImage), imagesy($ocImage));
         }
 
-        $fontFile = __DIR__ . '/../../assets/badges/' . $data->font;
+        $fontFile = __DIR__.'/../../assets/badges/'.$data->font;
         if (file_exists($fontFile) && $attendee->getNickName()) {
             $fontSize = $this->getMaxFontSize($fontFile, $attendee->getNickName(), $data->nameWidth, $data->nameHeight);
-            $color = imagecolorallocate($image, 50, 50, 50);
+            $color    = imagecolorallocate($image, 50, 50, 50);
             if ($color === false) {
                 throw new InvalidInputException('Could not allocate color for nickname text');
             }
@@ -110,13 +116,13 @@ class BadgeGenerator
             $boxSize = imagettfbbox($fontSize, 0, $fontFile, $attendee->getNickName());
             if ($boxSize === false) {
                 throw new InvalidInputException(
-                    'Could not calculate text box size for nickname: ' . $attendee->getNickName()
+                    'Could not calculate text box size for nickname: '.$attendee->getNickName()
                 );
             }
-            $width = $boxSize[2] - $boxSize[0];
+            $width  = $boxSize[2] - $boxSize[0];
             $height = $boxSize[1] - $boxSize[7];
-            $posX = (int)round(($data->nameWidth - $width) / 2) + $data->nameX;
-            $posY = (int)round(($data->nameHeight - $height) / 2) + $data->nameY + $fontSize;
+            $posX   = (int)round(($data->nameWidth - $width) / 2) + $data->nameX;
+            $posY   = (int)round(($data->nameHeight - $height) / 2) + $data->nameY + $fontSize;
 
             imagettftext($image, $fontSize, 0, $posX, $posY, $color, $fontFile, $attendee->getNickName());
         }
@@ -126,10 +132,10 @@ class BadgeGenerator
             throw new InvalidInputException('Could not create QR code image from data matrix');
         }
 
-        $qrWidth = imagesx($qrCode);
+        $qrWidth  = imagesx($qrCode);
         $qrHeight = imagesy($qrCode);
-        $posX = $data->qrX - $qrWidth;
-        $posY = $data->qrY - $qrHeight;
+        $posX     = $data->qrX - $qrWidth;
+        $posY     = $data->qrY - $qrHeight;
         imagecopy($image, $qrCode, $posX, $posY, 0, 0, $qrWidth, $qrHeight);
 
         $filename = $this->getFileName($attendee);
@@ -155,22 +161,22 @@ class BadgeGenerator
 
     private function resizeImage(GdImage $image, int $max_width, int $max_height): GdImage
     {
-        $orig_width = imagesx($image);
+        $orig_width  = imagesx($image);
         $orig_height = imagesy($image);
 
-        $width = $orig_width;
+        $width  = $orig_width;
         $height = $orig_height;
 
         # taller
         if ($height > $max_height) {
-            $width = (int)round(($max_height / $height) * $width);
+            $width  = (int)round(($max_height / $height) * $width);
             $height = $max_height;
         }
 
         # wider
         if ($width > $max_width) {
             $height = (int)round(($max_width / $width) * $height);
-            $width = $max_width;
+            $width  = $max_width;
         }
 
         $image_p = imagecreatetruecolor($width, $height);
@@ -230,7 +236,7 @@ class BadgeGenerator
 
     public function getFileName(Attendee $attendee): string
     {
-        $file = Path::join($this->projectDir, '/var/badges/', $attendee->getId() . '.png');
+        $file = Path::join($this->projectDir, '/var/badges/', $attendee->getId().'.png');
         $this->filesystem->mkdir(Path::getDirectory($file));
         return $file;
     }
